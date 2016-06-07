f2de8245a0d1a497b0145aa2e07e8b61386a8459
Léo LE BRAS <leo.lebrasf@gmail.com>
2016-06-07 13:50:17 +0200

    feat(react-native-example): add app example

---
+++ react-native-example/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+class App extends Component {
+  render() {
+    return (
+      <View style={{ marginTop: 40 }}>
+        <Text>Hello World !</Text>
+      </View>
+    )
+  }
+}
+
+export default App
